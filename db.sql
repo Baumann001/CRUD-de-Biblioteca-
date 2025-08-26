839b65e4649ac609aa0e291d495d3f589c1f669c
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2025-08-26 12:08:14 -0300

    add-

--- db.sql
+++ db.sql
@@ -1,2 +1,27 @@
 CREATE DATABASE  biblioteca_baumann;
 USE biblioteca_baumann;
+
+CREATE TABLE autores(
+
+id_autor INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+nacionalidade VARCHAR(100) NOT NULL,
+ano_nascimento DATE
+);
+
+CREATE TABLE livros(
+
+    id_livro INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100),
+    genero VARCHAR(100),
+    ano_publicacao DATE,
+    id_autor INT,
+    FOREIGN KEY (id_autor) REFERENCES autores (id_autor)
+);
+
+CREATE TABLE leitores(
+    id_leitor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    telefone VARCHAR(20) NOT NULL
+);
